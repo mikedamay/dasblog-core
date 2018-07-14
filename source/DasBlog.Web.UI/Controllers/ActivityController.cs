42e666915de9b1d6f8e785f38c6141759c87c1e7
mike <mikemay@blueyonder.co.uk>
2018-07-14 13:14:12 +0100

    scafolding for activity report

---
+++ source/DasBlog.Web.UI/Controllers/ActivityController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DasBlog.Web.Controllers
+{
+	[Authorize]
+	public class ActivityController : Controller
+	{
+		// GET
+		public IActionResult List()
+		{
+			return View();
+		}
+	}
+}
